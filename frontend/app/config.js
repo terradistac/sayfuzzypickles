8be5eba8eee753ec19c947e6deceb64d4f9bcea9
Christina Terradista <terradistacc@gmail.com>
2018-07-20 14:03:53 -0400

    Fixed path issue

--- frontend/app/config.js
+++ frontend/app/config.js
@@ -1,6 +1,6 @@
 module.exports = function (app, express) {
-    app.use('/js', express.static('node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-    app.use('/js', express.static('node_modules/jquery/dist')); // redirect JS jQuery
-    app.use('/css', express.static('node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-    app.use('/public', express.static('public'));
+    app.use('/js', express.static(__dirname + '/../node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+    app.use('/js', express.static(__dirname + '/../node_modules/jquery/dist')); // redirect JS jQuery
+    app.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+    app.use('/public', express.static(__dirname + '/../public'));
 }
